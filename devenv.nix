532cb63e023326dd6dd191ca4282c6c770fc3850
jwills801 <jwills@zagmail.gonzaga.edu>
2025-01-24 18:37:32 -0600

    Initialize devenv, make it include wine, and use wine to install FEMM

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+  env.WINEPREFIX = "/home/jackson/Documents/Courses/Year5/Design_Of_Rotating_Electric_Machines/wineprefix";
+  env.WINEARCH = "win64";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git
+              pkgs.wineWowPackages.stable ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+    scripts.femm = {
+    description = "Run FEMM using Wine";
+    exec = "wine $WINEPREFIX/drive_c/Program\\ Files/FEMM42/bin/femm.exe";
+  };
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
